08c4315250cc184e934cea18587d81dffdc53445
Den Shu <denis_shuleikin@epam.com>
2023-11-20 18:35:05 +0300

    function running and execution

--- main.go
+++ main.go
@@ -2,102 +2,205 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/sashabaranov/go-openai"
 	"os"
+	"os/exec"
 )
 
 var (
-	token      = os.Getenv("OPENAI_API_KEY")
-	system     = "I want you to act as a linux terminal commands assistant. You knows only file system operations only. You can't provide answer with commands that can modify or delete files. Only view. Do not write explanations. Just write commands."
-	question01 = "In which directory I am?"
-	question02 = "Create new directory call test-folder"
-	question03 = "Provide information about system?"
-	question04 = "List all files and provide size of all files in current folder"
+	token = os.Getenv("OPENAI_API_KEY")
+
+	systemPrompt = `You are a linux terminal commands explainer. User will provide you question, terminal command that he run and terminal output. You need to answer his question as short as possible. Is FORBIDDEN_COMMAND, means that user try to run command that can change something in system. You need to explain that, as assistant you cant execute commands that can modify something in system. Very shortly explain what can modify in system this command.
+`
+
+	systemFunctionPrompt = `I want you to act as a linux terminal commands assistant, who knows file system operations only. If users asks about adding, modifying or delete something, answer with FORBIDDEN_COMMAND with arguments of command and arguments.
+For example:
+Question: Delete all files in current directory
+FunctionCall: {"command":"FORBIDDEN_COMMAND","args":"rm -rf *"}
+
+Question: Move files from current directory to root
+FunctionCall: {"command":"FORBIDDEN_COMMAND","args":"mv * /"}
+
+Question: List all .txt files in current directory
+FunctionCall: {"command":"ls","args":"*.txt"}
+`
+
+	question01     = "In which directory I am?"
+	question02     = "Create new directory call test-folder"
+	question03     = "in current folder list all files with it sizes. calculate total size of all files"
+	questionsArray = []string{question01, question02, question03}
+
+	promptWithTerminalOutput = `Answer user question, using provided terminal output
+	Question: %v
+	Command: %v
+	Output: %v`
 )
 
 const (
-	model = openai.GPT4TurboPreview
+	model  = openai.GPT4TurboPreview
+	myFunc = `[{
+    "name": "get_terminal_command_with_args",
+	"description": "Provide linux/mac terminal command with arguments to run. If command is not view only and can modify something, return command: 'FORBIDDEN_COMMAND' with command and arguments.",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "command": {
+          "type": "string",
+           "description": "existing linux/mac terminal command"
+        },
+        "args": {
+          "description": "array of arguments for running with commands",
+          "type": "string"
+        }
+      },
+      "required": [
+        "command"
+      ]
+    }
+  }]
+`
 )
 
+// getFunction parse json string and return openai.Tool
+func getFunction(funcToParse string) ([]openai.Tool, error) {
+	var funcDef []openai.FunctionDefinition
+	err := json.Unmarshal([]byte(funcToParse), &funcDef)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshal function json: %v", err)
+	}
+
+	var toolFunc []openai.Tool
+	for _, f := range funcDef {
+		toolFunc = append(toolFunc, openai.Tool{
+			Type:     openai.ToolTypeFunction,
+			Function: f,
+		})
+	}
+	return toolFunc, nil
+}
+
 func main() {
-	//Reading env variables OPENAI_API_KEY and OPENAI_ORG_ID
+	//Initialisation. Reading env variables OPENAI_API_KEY and OpenAI Client
 	if token == "" {
 		fmt.Println("OPENAI_API_KEY environment variable is not set")
 		os.Exit(1)
 	}
-
-	//Init OpenAI client
 	client := openai.NewClient(token)
 
-	//Create chat completion request
+	//Parsing GPT function
+	gptFunction, err := getFunction(myFunc)
+	if err != nil {
+		fmt.Printf("error parsing function: %v\n", err)
+		os.Exit(1)
+	}
+
+	//Setting up function call
+	chatFunctionRequest := openai.ChatCompletionRequest{
+		Model:       model,
+		Tools:       gptFunction,
+		ToolChoice:  gptFunction[0],
+		Temperature: 0,
+	}
+
+	//Create function chat completion request
 	chatRequest := openai.ChatCompletionRequest{
 		Model: model,
 		Messages: []openai.ChatCompletionMessage{
 			{
 				Role:    openai.ChatMessageRoleSystem,
-				Content: system,
-			},
-			{
-				Role:    openai.ChatMessageRoleUser,
-				Content: question01,
+				Content: systemPrompt,
 			},
 		},
 	}
 
-	fmt.Printf("\nsystem: %v\n\n", chatRequest.Messages[0].Content)
-	fmt.Printf("user: %v\n\n", chatRequest.Messages[1].Content)
+	fmt.Printf("\nSYSTEM CHAT PROMPT: %v\n", systemPrompt)
+	fmt.Printf("SYSTEM FUNCTION PROMPT: %v\n", systemFunctionPrompt)
+	fmt.Printf("*****\n\n")
 
-	//Send request to OpenAI API
-	resp, err := client.CreateChatCompletion(context.Background(), chatRequest)
-	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
-		return
+	//let's iterate over our question
+	for i, userQuestion := range questionsArray {
+
+		fmt.Printf("Question %v. %v\n", i+1, userQuestion)
+		//Switching user question
+		//chatFunctionRequest.Messages[1].Content = userQuestion
+		chatFunctionRequest.Messages = []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: systemFunctionPrompt,
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: userQuestion,
+			},
+		}
+
+		//Calling GPT function
+		fmt.Printf("function call in progress...\n")
+		funcResp, err := client.CreateChatCompletion(context.Background(), chatFunctionRequest)
+		if err != nil {
+			fmt.Printf("Question %v. ChatCompletion error: %v\n", i, err)
+			os.Exit(1)
+		}
+		fmt.Printf("assistant: '%v'\n", funcResp.Choices[0].Message.Content)
+		fmt.Printf("function call: %+v\n", funcResp.Choices[0].Message.ToolCalls[0].Function)
+
+		//handling function response
+		//binding function response to map
+		var cmdMap map[string]string
+		err = json.Unmarshal([]byte(funcResp.Choices[0].Message.ToolCalls[0].Function.Arguments), &cmdMap)
+		if err != nil {
+			fmt.Printf("error unmarshal function json: %v\n", err)
+			os.Exit(1)
+		}
+
+		//running our functions called from GPT
+		getExecOutput, err := execute(cmdMap["command"], cmdMap["args"])
+		if err != nil {
+			fmt.Printf("error execute command: %v\n", err)
+			os.Exit(1)
+		}
+
+		//putting execution output to user question, and asking for providing answer
+		queryWithOutput := fmt.Sprintf(
+			promptWithTerminalOutput,
+			userQuestion,
+			fmt.Sprintf("%v %v", cmdMap["command"], cmdMap["args"]),
+			getExecOutput)
+		fmt.Printf("\nGPTChat call starts...\n")
+		fmt.Printf("user: %v\n", queryWithOutput)
+
+		chatRequest.Messages = append(chatRequest.Messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleUser,
+			Content: queryWithOutput,
+		})
+		chatResp, err := client.CreateChatCompletion(context.Background(), chatRequest)
+		if err != nil {
+			fmt.Printf("ChatCompletion error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("response from assistant: %v\n------\n", chatResp.Choices[0].Message.Content)
+		//adding response to chatRequest messages
+		chatRequest.Messages = append(chatRequest.Messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: chatResp.Choices[0].Message.Content,
+		})
 	}
-	//append GPT answer to messages
-	fmt.Printf("assistant: %v\n-----\n", resp.Choices[0].Message.Content)
-	chatRequest.Messages = append(chatRequest.Messages, resp.Choices[0].Message)
+}
 
-	//Adding second question to ChatGPT messages list
-	fmt.Printf("user: %v\n\n", question02)
-	chatRequest.Messages = append(chatRequest.Messages, openai.ChatCompletionMessage{
-		Role:    openai.ChatMessageRoleUser,
-		Content: question02,
-	})
-	resp, err = client.CreateChatCompletion(context.Background(), chatRequest)
-	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
-		os.Exit(1)
+func execute(command, params string) (string, error) {
+	var (
+		cmd *exec.Cmd
+	)
+	if command == "FORBIDDEN_COMMAND" {
+		return "FORBIDDEN_COMMAND", nil
 	}
-	fmt.Printf("assistant: %v\n-----\n", resp.Choices[0].Message.Content)
-	chatRequest.Messages = append(chatRequest.Messages, resp.Choices[0].Message)
-
-	//Third question to ChatGPT
-	fmt.Printf("user: %v\n\n", question03)
-	chatRequest.Messages = append(chatRequest.Messages, openai.ChatCompletionMessage{
-		Role:    openai.ChatMessageRoleUser,
-		Content: question03,
-	})
-	resp, err = client.CreateChatCompletion(context.Background(), chatRequest)
-	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
-		os.Exit(1)
+	if params != "" {
+		cmd = exec.Command(command, params)
+	} else {
+		cmd = exec.Command(command)
 	}
-	fmt.Printf("assistant: %v\n-----\n", resp.Choices[0].Message.Content)
-	chatRequest.Messages = append(chatRequest.Messages, resp.Choices[0].Message)
-
-	//Fourth question to ChatGPT
-	fmt.Printf("user: %v\n\n", question04)
-	chatRequest.Messages = append(chatRequest.Messages, openai.ChatCompletionMessage{
-		Role:    openai.ChatMessageRoleUser,
-		Content: question04,
-	})
-
-	resp, err = client.CreateChatCompletion(context.Background(), chatRequest)
-	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("assistant: %v\n-----\n", resp.Choices[0].Message.Content)
-	chatRequest.Messages = append(chatRequest.Messages, resp.Choices[0].Message)
+	output, err := cmd.Output()
+	return string(output), err
 }
